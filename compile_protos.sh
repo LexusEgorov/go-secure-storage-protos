a57cd38776c152e5186ae877372463f77efd9305
lexusegorov <alexey.egorov.job@gmail.com>
2025-04-11 21:54:16 +0700

    initial commit

---
+++ compile_protos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script generates source codes from .proto files
+# using Protobuf compiler protoc.
+
+protoc --go_out=./gen/golang \
+  --go-grpc_out=./gen/golang \
+  proto/**/*.proto
+sleep 2
